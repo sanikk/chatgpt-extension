cb3a632180febb1f286c8644589b25e88e016878
Samuli Nikkilä <sanikk@users.noreply.github.com>
2026-03-02 07:37:57 +0200

    added license headers
    updated:
    background.js
    content.js
    sidebar.css

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
+/*
+Copyright 2026 Samuli Nikkilä
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+// Background script to pass messages between frontend and backend.
+// This is run automatically by Firefox when the extension is enabled.
+
 browser.runtime.onMessage.addListener(async (msg) => {
     if (msg.type === "getTags") {
         const res = await fetch("http://localhost:8000/tags");
